6182021964d36a41af9d2dee8a71b3c5239af441
while208 <while208@126.com>
2015-03-25 16:26:15 +0800

    2
    2

---
+++ 3Dgis.cpp
@@ -0,0 +1,275 @@
+// 这段 MFC 示例源代码演示如何使用 MFC Microsoft Office Fluent 用户界面 
+// (“Fluent UI”)。该示例仅供参考，
+// 用以补充《Microsoft 基础类参考》和 
+// MFC C++ 库软件随附的相关电子文档。
+// 复制、使用或分发 Fluent UI 的许可条款是单独提供的。
+// 若要了解有关 Fluent UI 许可计划的详细信息，请访问  
+// http://msdn.microsoft.com/officeui。
+//
+// 版权所有(C) Microsoft Corporation
+// 保留所有权利。
+
+// 3Dgis.cpp : 定义应用程序的类行为。
+//
+
+#include "stdafx.h"
+#include "afxwinappex.h"
+#include "afxdialogex.h"
+#include "3Dgis.h"
+#include "MainFrm.h"
+
+#include "IpFrame.h"
+#include "3DgisDoc.h"
+#include "3DgisView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMy3DgisApp
+
+BEGIN_MESSAGE_MAP(CMy3DgisApp, CWinAppEx)
+	ON_COMMAND(ID_APP_ABOUT, &CMy3DgisApp::OnAppAbout)
+	// 基于文件的标准文档命令
+	ON_COMMAND(ID_FILE_NEW, &CWinAppEx::OnFileNew)
+	ON_COMMAND(ID_FILE_OPEN, &CWinAppEx::OnFileOpen)
+END_MESSAGE_MAP()
+
+
+// CMy3DgisApp 构造
+
+CMy3DgisApp::CMy3DgisApp()
+{
+	m_bHiColorIcons = TRUE;
+
+	// 支持重新启动管理器
+	m_dwRestartManagerSupportFlags = AFX_RESTART_MANAGER_SUPPORT_ALL_ASPECTS;
+#ifdef _MANAGED
+	// 如果应用程序是利用公共语言运行时支持(/clr)构建的，则:
+	//     1) 必须有此附加设置，“重新启动管理器”支持才能正常工作。
+	//     2) 在您的项目中，您必须按照生成顺序向 System.Windows.Forms 添加引用。
+	System::Windows::Forms::Application::SetUnhandledExceptionMode(System::Windows::Forms::UnhandledExceptionMode::ThrowException);
+#endif
+
+	// TODO: 将以下应用程序 ID 字符串替换为唯一的 ID 字符串；建议的字符串格式
+	//为 CompanyName.ProductName.SubProduct.VersionInformation
+	SetAppID(_T("3Dgis.AppID.NoVersion"));
+
+	// TODO: 在此处添加构造代码，
+	// 将所有重要的初始化放置在 InitInstance 中
+}
+
+// 唯一的一个 CMy3DgisApp 对象
+
+CMy3DgisApp theApp;
+// 生成的此标识符在统计上对于您的应用程序是唯一的
+// 如果您更愿选择一个特定的标识符，则可以更改它
+
+// {F27CC82D-8018-4563-B34B-3877EFE73A5C}
+static const CLSID clsid =
+{ 0xF27CC82D, 0x8018, 0x4563, { 0xB3, 0x4B, 0x38, 0x77, 0xEF, 0xE7, 0x3A, 0x5C } };
+
+
+// CMy3DgisApp 初始化
+
+BOOL CMy3DgisApp::InitInstance()
+{
+	// 如果一个运行在 Windows XP 上的应用程序清单指定要
+	// 使用 ComCtl32.dll 版本 6 或更高版本来启用可视化方式，
+	//则需要 InitCommonControlsEx()。否则，将无法创建窗口。
+	INITCOMMONCONTROLSEX InitCtrls;
+	InitCtrls.dwSize = sizeof(InitCtrls);
+	// 将它设置为包括所有要在应用程序中使用的
+	// 公共控件类。
+	InitCtrls.dwICC = ICC_WIN95_CLASSES;
+	InitCommonControlsEx(&InitCtrls);
+
+	CWinAppEx::InitInstance();
+	afxAmbientActCtx = FALSE;
+
+	if (!AfxSocketInit())
+	{
+		AfxMessageBox(IDP_SOCKETS_INIT_FAILED);
+		return FALSE;
+	}
+
+	// 初始化 OLE 库
+	if (!AfxOleInit())
+	{
+		AfxMessageBox(IDP_OLE_INIT_FAILED);
+		return FALSE;
+	}
+
+	AfxEnableControlContainer();
+
+	EnableTaskbarInteraction(FALSE);
+
+	// 使用 RichEdit 控件需要  AfxInitRichEdit2()	
+	// AfxInitRichEdit2();
+
+	// 标准初始化
+	// 如果未使用这些功能并希望减小
+	// 最终可执行文件的大小，则应移除下列
+	// 不需要的特定初始化例程
+	// 更改用于存储设置的注册表项
+	// TODO: 应适当修改该字符串，
+	// 例如修改为公司或组织名
+	SetRegistryKey(_T("应用程序向导生成的本地应用程序"));
+	LoadStdProfileSettings(4);  // 加载标准 INI 文件选项(包括 MRU)
+
+
+	InitContextMenuManager();
+	InitShellManager();
+
+	InitKeyboardManager();
+
+	InitTooltipManager();
+	CMFCToolTipInfo ttParams;
+	ttParams.m_bVislManagerTheme = TRUE;
+	theApp.GetTooltipManager()->SetTooltipParams(AFX_TOOLTIP_TYPE_ALL,
+		RUNTIME_CLASS(CMFCToolTipCtrl), &ttParams);
+
+	// 注册应用程序的文档模板。文档模板
+	// 将用作文档、框架窗口和视图之间的连接
+	CSingleDocTemplate* pDocTemplate;
+	pDocTemplate = new CSingleDocTemplate(
+		IDR_MAINFRAME,
+		RUNTIME_CLASS(CMy3DgisDoc),
+		RUNTIME_CLASS(CMainFrame),       // 主 SDI 框架窗口
+		RUNTIME_CLASS(CMy3DgisView));
+	if (!pDocTemplate)
+		return FALSE;
+	pDocTemplate->SetContainerInfo(IDR_CNTR_INPLACE);
+	pDocTemplate->SetServerInfo(
+		IDR_SRVR_EMBEDDED, IDR_SRVR_INPLACE,
+		RUNTIME_CLASS(CInPlaceFrame));
+	AddDocTemplate(pDocTemplate);
+	// 将 COleTemplateServer 连接到文档模板
+	// COleTemplateServer 通过使用
+	// 文档模板中指定的信息来为请求 OLE 容器
+	// 创建新文档
+	m_server.ConnectTemplate(clsid, pDocTemplate, TRUE);
+		// 注意: 仅当命令行中存在 /Embedding 或 /Automation 时，SDI 应用程序
+		//   才注册服务器对象
+
+
+	// 分析标准 shell 命令、DDE、打开文件操作的命令行
+	CCommandLineInfo cmdInfo;
+	ParseCommandLine(cmdInfo);
+
+	// 启用“DDE 执行”
+	EnableShellOpen();
+	RegisterShellFileTypes(TRUE);
+
+	// 应用程序是用 /Embedding 或 /Automation 开关启动的。
+	//使应用程序作为自动化服务器运行。
+	if (cmdInfo.m_bRunEmbedded || cmdInfo.m_bRunAutomated)
+	{
+		// 将所有的 OLE 服务器工厂注册为正在运行。这将启用
+		//  OLE 库以从其他应用程序中创建对象
+		COleTemplateServer::RegisterAll();
+
+		// 不显示主窗口
+		return TRUE;
+	}
+	// 使用 /Unregserver 或 /Unregister 开关启动应用程序。注销
+	// 类型库。其他注销操作在 ProcessShellCommand() 中发生。
+	else if (cmdInfo.m_nShellCommand == CCommandLineInfo::AppUnregister)
+	{
+		UnregisterShellFileTypes();
+		m_server.UpdateRegistry(OAT_INPLACE_SERVER, NULL, NULL, FALSE);
+	}
+	// 应用程序是以独立方式或用其他开关(如 /Register
+	// 或 /Regserver)启动的。更新注册表项，包括类型库。
+	else
+	{
+		m_server.UpdateRegistry(OAT_INPLACE_SERVER);
+	}
+
+	// 调度在命令行中指定的命令。如果
+	// 用 /RegServer、/Register、/Unregserver 或 /Unregister 启动应用程序，则返回 FALSE。
+	if (!ProcessShellCommand(cmdInfo))
+		return FALSE;
+
+	// 唯一的一个窗口已初始化，因此显示它并对其进行更新
+	m_pMainWnd->ShowWindow(SW_SHOW);
+	m_pMainWnd->UpdateWindow();
+	// 仅当具有后缀时才调用 DragAcceptFiles
+	//  在 SDI 应用程序中，这应在 ProcessShellCommand 之后发生
+	// 启用拖/放
+	m_pMainWnd->DragAcceptFiles();
+	return TRUE;
+}
+
+int CMy3DgisApp::ExitInstance()
+{
+	//TODO: 处理可能已添加的附加资源
+	AfxOleTerm(FALSE);
+
+	return CWinAppEx::ExitInstance();
+}
+
+// CMy3DgisApp 消息处理程序
+
+
+// 用于应用程序“关于”菜单项的 CAboutDlg 对话框
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 对话框数据
+	enum { IDD = IDD_ABOUTBOX };
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+// 实现
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(CAboutDlg::IDD)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+// 用于运行对话框的应用程序命令
+void CMy3DgisApp::OnAppAbout()
+{
+	CAboutDlg aboutDlg;
+	aboutDlg.DoModal();
+}
+
+// CMy3DgisApp 自定义加载/保存方法
+
+void CMy3DgisApp::PreLoadState()
+{
+	BOOL bNameValid;
+	CString strName;
+	bNameValid = strName.LoadString(IDS_EDIT_MENU);
+	ASSERT(bNameValid);
+	GetContextMenuManager()->AddMenu(strName, IDR_POPUP_EDIT);
+}
+
+void CMy3DgisApp::LoadCustomState()
+{
+}
+
+void CMy3DgisApp::SaveCustomState()
+{
+}
+
+// CMy3DgisApp 消息处理程序
+
+
+
---
+++ 3DgisDoc.cpp
@@ -0,0 +1,177 @@
+// 这段 MFC 示例源代码演示如何使用 MFC Microsoft Office Fluent 用户界面 
+// (“Fluent UI”)。该示例仅供参考，
+// 用以补充《Microsoft 基础类参考》和 
+// MFC C++ 库软件随附的相关电子文档。
+// 复制、使用或分发 Fluent UI 的许可条款是单独提供的。
+// 若要了解有关 Fluent UI 许可计划的详细信息，请访问  
+// http://msdn.microsoft.com/officeui。
+//
+// 版权所有(C) Microsoft Corporation
+// 保留所有权利。
+
+// 3DgisDoc.cpp : CMy3DgisDoc 类的实现
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "3Dgis.h"
+#endif
+
+#include "3DgisDoc.h"
+#include "CntrItem.h"
+#include "SrvrItem.h"
+
+#include <propkey.h>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+// CMy3DgisDoc
+
+IMPLEMENT_DYNCREATE(CMy3DgisDoc, COleServerDoc)
+
+BEGIN_MESSAGE_MAP(CMy3DgisDoc, COleServerDoc)
+	// 启用默认的 OLE 容器实现
+	ON_UPDATE_COMMAND_UI(ID_EDIT_PASTE, &COleServerDoc::OnUpdatePasteMenu)
+	ON_UPDATE_COMMAND_UI(ID_EDIT_PASTE_LINK, &COleServerDoc::OnUpdatePasteLinkMenu)
+	ON_UPDATE_COMMAND_UI(ID_OLE_EDIT_CONVERT, &COleServerDoc::OnUpdateObjectVerbMenu)
+	ON_COMMAND(ID_OLE_EDIT_CONVERT, &COleServerDoc::OnEditConvert)
+	ON_UPDATE_COMMAND_UI(ID_OLE_EDIT_LINKS, &COleServerDoc::OnUpdateEditLinksMenu)
+	ON_COMMAND(ID_OLE_EDIT_LINKS, &COleServerDoc::OnEditLinks)
+	ON_UPDATE_COMMAND_UI_RANGE(ID_OLE_VERB_FIRST, ID_OLE_VERB_LAST, &COleServerDoc::OnUpdateObjectVerbMenu)
+END_MESSAGE_MAP()
+
+
+// CMy3DgisDoc 构造/析构
+
+CMy3DgisDoc::CMy3DgisDoc()
+{
+	// 使用 OLE 复合文件
+	EnableCompoundFile();
+
+	// TODO: 在此添加一次性构造代码
+
+}
+
+CMy3DgisDoc::~CMy3DgisDoc()
+{
+}
+
+BOOL CMy3DgisDoc::OnNewDocument()
+{
+	if (!COleServerDoc::OnNewDocument())
+		return FALSE;
+
+	// TODO: 在此添加重新初始化代码
+	// (SDI 文档将重用该文档)
+
+	return TRUE;
+}
+
+
+// CMy3DgisDoc 服务器实现
+
+COleServerItem* CMy3DgisDoc::OnGetEmbeddedItem()
+{
+	// OnGetEmbeddedItem 由框架调用以获取与该文档关联的 COleServerItem。
+	//  只在必要时才调用它。
+
+	CMy3DgisSrvrItem* pItem = new CMy3DgisSrvrItem(this);
+	ASSERT_VALID(pItem);
+	return pItem;
+}
+
+
+
+
+// CMy3DgisDoc 序列化
+
+void CMy3DgisDoc::Serialize(CArchive& ar)
+{
+	if (ar.IsStoring())
+	{
+		// TODO: 在此添加存储代码
+	}
+	else
+	{
+		// TODO: 在此添加加载代码
+	}
+
+	// 调用基类 COleServerDoc 将启用
+	//  容器文档的 COleClientItem 对象的序列化。
+	COleServerDoc::Serialize(ar);
+}
+
+#ifdef SHARED_HANDLERS
+
+// 缩略图的支持
+void CMy3DgisDoc::OnDrawThumbnail(CDC& dc, LPRECT lprcBounds)
+{
+	// 修改此代码以绘制文档数据
+	dc.FillSolidRect(lprcBounds, RGB(255, 255, 255));
+
+	CString strText = _T("TODO: implement thumbnail drawing here");
+	LOGFONT lf;
+
+	CFont* pDefaultGUIFont = CFont::FromHandle((HFONT) GetStockObject(DEFAULT_GUI_FONT));
+	pDefaultGUIFont->GetLogFont(&lf);
+	lf.lfHeight = 36;
+
+	CFont fontDraw;
+	fontDraw.CreateFontIndirect(&lf);
+
+	CFont* pOldFont = dc.SelectObject(&fontDraw);
+	dc.DrawText(strText, lprcBounds, DT_CENTER | DT_WORDBREAK);
+	dc.SelectObject(pOldFont);
+}
+
+// 搜索处理程序的支持
+void CMy3DgisDoc::InitializeSearchContent()
+{
+	CString strSearchContent;
+	// 从文档数据设置搜索内容。
+	// 内容部分应由“;”分隔
+
+	// 例如:  strSearchContent = _T("point;rectangle;circle;ole object;")；
+	SetSearchContent(strSearchContent);
+}
+
+void CMy3DgisDoc::SetSearchContent(const CString& value)
+{
+	if (value.IsEmpty())
+	{
+		RemoveChunk(PKEY_Search_Contents.fmtid, PKEY_Search_Contents.pid);
+	}
+	else
+	{
+		CMFCFilterChunkValueImpl *pChunk = NULL;
+		ATLTRY(pChunk = new CMFCFilterChunkValueImpl);
+		if (pChunk != NULL)
+		{
+			pChunk->SetTextValue(PKEY_Search_Contents, value, CHUNK_TEXT);
+			SetChunkValue(pChunk);
+		}
+	}
+}
+
+#endif // SHARED_HANDLERS
+
+// CMy3DgisDoc 诊断
+
+#ifdef _DEBUG
+void CMy3DgisDoc::AssertValid() const
+{
+	COleServerDoc::AssertValid();
+}
+
+void CMy3DgisDoc::Dump(CDumpContext& dc) const
+{
+	COleServerDoc::Dump(dc);
+}
+#endif //_DEBUG
+
+
+// CMy3DgisDoc 命令
